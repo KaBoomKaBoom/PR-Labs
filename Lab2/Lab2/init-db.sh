3ef652a9c65ebbf514aab962c4b7fdfddb2d537b
Berco Andrei <andrei2003berco@gmail.com>
2024-10-25 22:10:36 +0300

    Fix connnection to SQL server

--- Lab2/Lab2/init-db.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Wait for SQL Server to start
-echo "Waiting for SQL Server to start..."
-sleep 10s
-
-# Run the SQL script to create the database and tables
-/opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P "#1324aAa1324" -d master -i /usr/src/app/init.sql
-
